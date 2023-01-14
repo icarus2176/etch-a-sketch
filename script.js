9cbf2098612a018d2d3ff16ecd1455a913429534
icarus2176 <ashe.kemuri@gmail.com>
2023-01-14 15:00:24 -0600

    add makeGrid function

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ gridSize.addEventListener('click',askSize);
 const grid = document.querySelector('#grid');
 
 function askSize(){
-    const size = parseint(prompt("How many boxes do you want on each side (up to 100)?"));
+    const size = (prompt("How many boxes do you want on each side (up to 100)?"));
     makeGrid(size);
 }
 
 function makeGrid(size){
-    
+    const currentRows = Array.from(document.querySelectorAll('.row'));
+    currentRows.forEach(row => row.remove());
+    const row = [size]
+    for(let i = 0; i < size; i++){
+        row[i] = document.createElement('div');
+        row[i].classList.add('row');
+        grid.appendChild(row[i]);
+
+        for(let j = 0; j < size; j++){
+            const eachBox = [size];
+            eachBox[j] = document.createElement('div');
+            eachBox[j].classList.add('box');
+            row[i].appendChild(eachBox[j]);
+        }
+    }
 }
+
+makeGrid(16);
